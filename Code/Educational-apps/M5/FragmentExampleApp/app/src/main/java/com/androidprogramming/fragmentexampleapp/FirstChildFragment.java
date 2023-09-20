3fac2ae16669793e1950bdd7b3e4bbe3aae44ac8
kevinmala00 <kevinmala000@gmail.com>
2023-09-20 11:19:09 +0200

    add FragmentExampleApp

---
+++ Code/Educational-apps/M5/FragmentExampleApp/app/src/main/java/com/androidprogramming/fragmentexampleapp/FirstChildFragment.java
@@ -0,0 +1,6 @@
+package com.androidprogramming.fragmentexampleapp;
+
+import androidx.fragment.app.Fragment;
+
+public class FirstChildFragment extends Fragment {
+}
---
+++ Code/Educational-apps/M5/FragmentExampleApp/app/src/main/java/com/androidprogramming/fragmentexampleapp/NavigationFragment.java
@@ -0,0 +1,8 @@
+package com.androidprogramming.fragmentexampleapp;
+
+import androidx.fragment.app.Fragment;
+
+public class NavigationFragment extends Fragment {
+
+
+}
